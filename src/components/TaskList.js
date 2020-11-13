bac8e686ee497842de30619d1d8f7960d77b8ed1
MDShields7 <matthew.duncan.shields@gmail.com>
2020-11-12 21:45:05 -0600

    chore: "wire in data"

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
-
-import Task from "./Task";
 import PropTypes from "prop-types";
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import Task from "./Task";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
+
+export function PureTaskList({
+  loading,
+  tasks,
+  onPinTask,
+  onArchiveTask,
+}) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -53,7 +60,7 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
@@ -63,6 +70,19 @@ TaskList.propTypes = {
   /** Event to change the task to archived */
   onArchiveTask: PropTypes.func,
 };
-TaskList.defaultProps = {
+
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
